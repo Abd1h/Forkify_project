fbf4d59eb4d09ce2ecdd93055b6b2dbea69d8477
Abd1h <abdlh.lllh@gmail.com>
2022-07-16 21:37:23 +0300

    july16 endd

---
+++ src/js/Views/paginationView.js
@@ -0,0 +1,85 @@
+import { View } from "./View.js";
+import icons from 'url:../../img/icons.svg'
+
+
+class paginationView extends View {
+_parentEl = document.querySelector('.pagination')
+
+
+// _rednerMarkup(data = ''){
+    
+// return this._genMarkupPaginaiton()
+
+// }
+
+_rednerMarkup(){
+   
+const {currentPage} = this._data
+const numberOfPages = Math.ceil(this._data.results.length / this._data.resultsPerPage) //div number of results / results per page "10"
+
+
+// state 1 -> page 1 and there is no other pages
+if (currentPage === 1 && numberOfPages === 1){
+    return  `<button class="btn--inline pagination__btn--prev">
+    <svg class="search__icon">
+      <use href="${icons}#icon-arrow-left"></use>
+    </svg>
+    <span>Page ${currentPage - 1}</span>
+  </button>
+  <button class="btn--inline pagination__btn--next">
+    <span>Page ${currentPage + 1}</span>
+    <svg class="search__icon">
+      <use href=" ${icons}#icon-arrow-right"></use>
+    </svg>
+  </button>`
+}
+// state 2 -> page 1 and there is other pages
+if (currentPage === 1 && numberOfPages > 1){
+    return  `<button class="btn--inline pagination__btn--prev">
+    <svg class="search__icon">
+      <use href="${icons}#icon-arrow-left"></use>
+    </svg>
+    <span>Page ${currentPage - 1}</span>
+  </button>
+  <button class="btn--inline pagination__btn--next">
+    <span>Page ${currentPage + 1}</span>
+    <svg class="search__icon">
+      <use href=" ${icons}#icon-arrow-right"></use>
+    </svg>
+  </button>`
+}
+// state 3 -> last page
+if (currentPage === numberOfPages ){
+    return  `<button class="btn--inline pagination__btn--prev">
+    <svg class="search__icon">
+      <use href="${icons}#icon-arrow-left"></use>
+    </svg>
+    <span>Page ${currentPage - 1}</span>
+  </button>
+  <button class="btn--inline pagination__btn--next">
+    <span>Page ${currentPage + 1}</span>
+    <svg class="search__icon">
+      <use href=" ${icons}#icon-arrow-right"></use>
+    </svg>
+  </button>`
+}
+// state 4 -> between other pages
+if (currentPage > 1 && currentPage < numberOfPages){
+    return  `<button class="btn--inline pagination__btn--prev">
+    <svg class="search__icon">
+      <use href="${icons}#icon-arrow-left"></use>
+    </svg>
+    <span>Page ${currentPage - 1}</span>
+  </button>
+  <button class="btn--inline pagination__btn--next">
+    <span>Page ${currentPage + 1}</span>
+    <svg class="search__icon">
+      <use href=" ${icons}#icon-arrow-right"></use>
+    </svg>
+  </button>`
+}
+
+}
+
+}
+export default new paginationView()
